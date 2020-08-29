c09ee441e3a2d3ba2ed1ae20c6332f46c5f059d6
yujinc0430 <68933481+yujinc0430@users.noreply.github.com>
2020-08-29 22:41:04 +0900

    0829
    0829_html 수정 파일

--- DjangoBookLibrary/book_store/settings.py
+++ DjangoBookLibrary/book_store/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'books.apps.BooksConfig',
     'users.apps.UsersConfig',
-    'crispy_forms'
+    'crispy_forms',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = 'book_store.urls'
 
--- DjangoBookLibrary/books/views.py
+++ DjangoBookLibrary/books/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.http import Http404, HttpResponseForbidden
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.utils.text import slugify
 from django.views.generic.edit import FormMixin
 from django.views.generic import (
     DetailView,
@@ -33,18 +34,23 @@ class HomeListView(ListView):
         return queryset.all().order_by('-id')[:9]
 
 
-class SearchBookListView(ListView):
-    template_name = "books/book_search_result.html"
-    model = Book
+def SearchBookListView(request):
 
-    def get_queryset(self):
-        queryset = super(SearchBookListView, self).get_queryset()
-        q = self.request.GET.get("q")
-        if q:
-            books_by_title = queryset.filter(title__icontains=q)
-            books_by_author = queryset.filter(author__icontains=q)
-            return books_by_author | books_by_title
-        return queryset
+    if request.method == 'POST':
+        book_form = Book()
+        book_form.id = int(request.POST['isbn'])
+        book_form.title = request.POST['title']
+        book_form.slug = slugify(int(request.POST['isbn']))
+        book_form.description = request.POST['contents']
+        book_form.author = request.POST['author']
+        book_form.publish_date = request.POST['publish_date']
+        book_form.save()
+        return redirect('bookDetail', slug=book_form.slug)
+
+    else:
+        pass
+
+    return render(request, "books/book_search_result.html")
 
 
 class BookDetailView(FormMixin, DetailView):
